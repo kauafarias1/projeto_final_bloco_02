4483a9face6c88f6fffd3bbcbb078f89fa42fa39
kaua <kauagabrielfarias18@gmail.com>
2025-12-17 12:37:55 -0300

    Implementação CRUD de Categoria

---
+++ src/categoria/services/categoria.service.ts
@@ -0,0 +1,55 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Categoria } from "../entities/categoria.entity";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { NotFoundException } from "@nestjs/common";
+
+export class CategoriaService {
+    constructor(
+        @InjectRepository(Categoria)
+        private categoriaRepository: Repository<Categoria>
+    ) {}
+
+    async findAll(): Promise<Categoria[]> {
+        return this.categoriaRepository.find();
+    }
+
+    async findById(id: number): Promise<Categoria> {
+        let categoria = await this.categoriaRepository.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!categoria) {
+            throw new NotFoundException(`Categoria ${id} não encontrada`);
+        }
+
+        return categoria
+    }
+
+    async findByNome(nome: string): Promise<Categoria[]> {
+        return await this.categoriaRepository.find({
+            where: {
+                nome: ILike(`%${nome}%`)
+            }
+        });
+    }
+
+    async create (categoria: Categoria): Promise<Categoria> {
+        return await this.categoriaRepository.save(categoria);
+    }
+
+    async update(categoria: Categoria): Promise<Categoria> {
+
+        await this.findById(categoria.id);
+
+        return await this.categoriaRepository.save(categoria);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        await this.findById(id);
+
+        return await this.categoriaRepository.delete(id);
+    }
+}
